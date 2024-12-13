ad2a790ef0359ca5273abf1a0c839fba60c320df
Ved Prakash Vishwakarma <vedvishwakarma226@gmail.com>
2024-12-13 14:49:40 +0530

    user model is created and mongo server setup in the db file

--- Backend/app.js
+++ Backend/app.js
@@ -3,11 +3,14 @@ dotenv.config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const connectToDb = require('./db/db');
+
+connectToDb();
 
 app.use(cors());
 
 app.get('/',(req, res)=>{
-    res.send('Hello World');
+    res.send('Hello World!');
 })
 
 module.exports = app;
